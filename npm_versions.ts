47d065bde11f8cc55885c8ef3be753deda54d9c6
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-08-24 15:13:26 +0200

    Npm version wip

--- npm_versions.ts
+++ npm_versions.ts
@@ -1,6 +1,33 @@
-const decoder = new TextDecoder()
-const file = await Deno.readFile("a.json")
-const f = decoder.decode(file)
+const getJsonFile = async () => {
+  const decoder = new TextDecoder()
+  const file = await Deno.readFile("a.json")
+  return decoder.decode(file)
+}
 
-const parsed = JSON.parse(f)
-console.log(parsed)
+const file = await getJsonFile()
+const parseJsonFile = (file: string) => {
+  try {
+    const {dependencies, devDependencies} = JSON.parse(file)
+    return {dependencies, devDependencies}
+  } catch (error) {
+    // @ts-ignore
+    console.error("Can't parse file", error.message)
+    return null
+  }
+}
+
+const packages = parseJsonFile(file)
+
+const getDependencies = (dependencies: Record<string, string>) => {
+  return Object.entries(dependencies).map(([title, currentVersion]) => ({
+    title,
+    currentVersion,
+    amiableStableVersion: "",
+  }))
+}
+if (packages !== null) {
+  const dep = getDependencies(packages.dependencies)
+  const devDep = getDependencies(packages.devDependencies)
+}
+
+// TODO use table library
