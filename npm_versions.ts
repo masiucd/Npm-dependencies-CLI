656dc6539e05b77e4858c3fe5e4ae06aa043ce87
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-08-28 11:37:56 +0200

    With oclifff

--- npm_versions.ts
+++ npm_versions.ts
@@ -1,9 +1,18 @@
 // import {z} from "https://deno.land/x/zod@v3.18.0/mod.ts"
-import {table} from "https://deno.land/x/minitable@v1.0/mod.ts"
+// import {table} from "https://deno.land/x/minitable@v1.0/mod.ts"
 import {exec} from "https://deno.land/x/execute@v1.1.0/mod.ts"
 import * as colors from "https://deno.land/std@0.153.0/fmt/colors.ts"
+import {Table} from "https://deno.land/x/cliffy@v0.24.3/table/mod.ts"
 
 // npm view {package} ---> to get latest version from shell or  yarn info {package} versions or npm show {package} version
+
+const tableW = new Table()
+  .header(["Title", "Current version", "Available Stable Version"])
+  .maxColWidth(70)
+  .padding(1)
+  .indent(2)
+  .border(true)
+// .render()
 
 const args = Deno.args
 
@@ -19,8 +28,8 @@ const parseJsonFile = (file: string) => {
     const {dependencies, devDependencies} = JSON.parse(file)
     return {dependencies, devDependencies}
   } catch (error) {
-    // @ts-ignore
-    console.error("Can't parse file", error.message)
+    const message = getErrorMessage(error)
+    console.error(`Can't parse file ${file}`, message)
     return null
   }
 }
@@ -30,27 +39,30 @@ const packages = parseJsonFile(file)
 const getDependencies = async (dependencies: Record<string, string>) =>
   await Promise.all(
     Object.entries(dependencies).map(async ([title, version]) => ({
-      Title: colors.bgBlue(colors.black(title)),
-      CurrentVersion: colors.bgBrightCyan(colors.black(version)),
-      AvailableStableVersion: colors.bgBrightGreen(colors.black(await getLatestVersion(title))),
+      Title: title,
+      CurrentVersion: version,
+      AvailableStableVersion: await getLatestVersion(title),
     }))
   )
 
 if (packages !== null) {
-  let dep
+  let dep: any
   if (args[0] === "dev") {
     dep = await getDependencies(packages.devDependencies)
   } else {
     dep = await getDependencies(packages.dependencies)
   }
-  const tableDependencies = table(dep, ["Title", "CurrentVersion", "AvailableStableVersion"], {
-    padding: 2,
-    upcaseHeader: false,
-    emptyReplacer: "Empty",
-  })
-  console.log(tableDependencies)
+  dep = dep.map((p: Record<string, string>) => Object.values(p))
+  tableW.body(dep)
 }
 
 async function getLatestVersion(title: string) {
   return await exec(`npm show ${title} version`)
+}
+
+tableW.render()
+
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) return error.message
+  return String(error)
 }
